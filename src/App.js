af977f7c53f66c9a50905c52e1edfaae8d31f927
Elie Dutheil <eliedthl@gmail.com>
2019-10-12 19:48:48 +0200

    ticket-10

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ import './App.css';
 import Sound from 'react-sound';
 import Button from './Button';
 
-const apiToken = 'BQBpJn3LKSwUhtgoPjihL1FJMQkIgg2GextoHJQUMp5omtD5dTo7PT994gn0O42_oUlzU-u-CMCKIwkpCsT7Q8ZOhPhQJJ_V8tbe15yRFTn7jciTodv-lDNFrgu11FnjOfbqx6dM4w69NGpwOXRfYK--21ndh-Fp7z5saNmL6vJEI-IHXvFGROc9SoUq8WUm39e5hUl582QxBdR_Cq4vQ5ayVhU81hDNryvdpzKLctIxp_NwnFj8Kq1aHqFwTQSUw_YItHrl6Z-KdVu37NljBA';
+const apiToken =
+  'BQBpJn3LKSwUhtgoPjihL1FJMQkIgg2GextoHJQUMp5omtD5dTo7PT994gn0O42_oUlzU-u-CMCKIwkpCsT7Q8ZOhPhQJJ_V8tbe15yRFTn7jciTodv-lDNFrgu11FnjOfbqx6dM4w69NGpwOXRfYK--21ndh-Fp7z5saNmL6vJEI-IHXvFGROc9SoUq8WUm39e5hUl582QxBdR_Cq4vQ5ayVhU81hDNryvdpzKLctIxp_NwnFj8Kq1aHqFwTQSUw_YItHrl6Z-KdVu37NljBA';
 
 function shuffleArray(array) {
   let counter = array.length;
@@ -29,38 +30,46 @@ function getRandomNumber(x) {
 }
 
 class App extends Component {
-
   constructor() {
     super();
+    this.state = {
+      areSongsLoaded: false
+    };
   }
 
   componentDidMount() {
     fetch('https://api.spotify.com/v1/me/tracks', {
       method: 'GET',
       headers: {
-      Authorization: 'Bearer ' + apiToken,
-      },
+        Authorization: 'Bearer ' + apiToken
+      }
     })
       .then(response => response.json())
-      .then((data) => {
+      .then(data => {
         console.log("Réponse reçue ! Voilà ce que j'ai reçu : ", data);
-      })
+        this.setState({
+          areSongsLoaded: true
+        });
+      });
   }
 
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo"/>
-          <h1 className="App-title">Bienvenue sur le Blindtest</h1>
-        </header>
-        <div className="App-images">
-          <p>Il va falloir modifier le code pour faire un vrai Blindtest !</p>
+    if (this.state.areSongsLoaded) {
+      return (
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Bienvenue sur le Blindtest</h1>
+          </header>
+          <div className="App-images">
+            <p>Il va falloir modifier le code pour faire un vrai Blindtest !</p>
+          </div>
+          <div className="App-buttons"></div>
         </div>
-        <div className="App-buttons">
-        </div>
-      </div>
-    );
+      );
+    } else {
+      return <img src={loading} className="loading" alt="loading" />;
+    }
   }
 }
 
